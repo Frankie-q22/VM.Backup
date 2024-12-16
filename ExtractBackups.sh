3373659fb81117a5c7f399907f27ad5aaf2be0af
Frankie-q22 <124313378+Frankie-q22@users.noreply.github.com>
2024-12-16 11:11:09 -0700

    Update ExtractBackups.sh

--- ExtractBackups.sh
+++ ExtractBackups.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-Backup_Directory='/home/student/Backup'
-Extracted_Directory='
+
+Backup_Directory="/home/student/Backup"
+Extracted_Directory="/home/student/Backup/Extracted_Backups"
+
+
+
+
+for file in "$Backup_Directory"/backup-*$time; do
+    if [ -f "$file" ]; then
+
+        tar -xzf "$file" -C "$Extracted_Directory"
+        echo "Extracted $file to $Extracted_Directory"
+else
+        echo "No backup files found in $Backup_Directory"
+    fi
+done
+
+
